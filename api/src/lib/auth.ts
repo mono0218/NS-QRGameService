6f24a8e83cf3a6d18701f65662302fab0162d95b
mono0218 <mono@monokamo.com>
2024-11-06 00:45:53 +0900

    Fix: HashAuth

--- api/src/lib/auth.ts
+++ api/src/lib/auth.ts
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken'
 import {HTTPException} from "hono/http-exception";
 import {PrismaClient} from "@prisma/client";
+import crypto from "crypto";
 
 const prisma = new PrismaClient()
 
@@ -18,10 +19,11 @@ export function userAuth(key:string|undefined):string{
 
 export async function serviceAuth(key:string|undefined){
     if (!key) throw new HTTPException(401, {message:'Unauthorized'})
+    const hashKey = crypto.createHash('sha256').update(key).digest('hex')
 
     const data = await prisma.service.findFirst({
         where: {
-            hashKey: key
+            hashKey: hashKey
         }
     })
 
